695efaa797fe1ab7ca0979981ac706f70a320be0
jkhoang313 <jkhoang313@gmail.com>
2017-01-31 09:29:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //Code React element here
 const header = React.createElement('h1', {}, "An Awesome Person")
 const paragraph = React.createElement('p', {}, "Who is learning React")
-const listItemOne = React.createElement('li', {}, "Javascript")
+const listItemOne = React.createElement('li', {}, "JavaScript")
 const listItemTwo = React.createElement('li', {}, "React")
 const listItemThree = React.createElement('li', {}, "Movies")
-const listItemFour = React.createElement('li', {}, "Ice Cream")
+const listItemFour = React.createElement('li', {}, "Ice cream")
 const list = React.createElement('ul', {className: "me_interests"}, [listItemOne, listItemTwo, listItemThree, listItemFour])
 const meInReact = React.createElement('div', {className: "me"}, [header, paragraph, list])
 
